1e52878cc29b099bd4d79f61acd3935fb1cb31da
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2025-03-06 23:57:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 648 ms, Memory: 66208 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,32 +1,28 @@
-#연결 요소의 개수 
-import sys 
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)  # 재귀 깊이 증가 
+import sys
 
-N, M = map(int, input().split()) #정점 개수, 간선 개수 
+sys.setrecursionlimit(10 ** 6)
 
-def dfs(graph, v, visited):
-    visited[v] = True
-    for i in graph[v]:
-        if visited[i]==False: 
-            dfs(graph, i, visited)
+N, M = map(int, sys.stdin.readline().strip().split())
 
+graph = [[] for _ in range(N + 1)]
 
-
-# 그래프 입력 (adjacency list 방식)
-graph = [[] for _ in range(N + 1)] 
 for _ in range(M):
-    u, v = map(int, input().split())
+    u, v = map(int, sys.stdin.readline().strip().split())
     graph[u].append(v)
     graph[v].append(u)
 
-visited = [False] * (N+1)
-cnt = 0
+connected_component = 0
+visited = [False] * (N + 1)
+
+def dfs(index):
+    visited[index] = True
+    for k in graph[index]:
+        if not visited[k]:
+            dfs(k)
 
 for i in range(1, N + 1):
-    if visited[i]==False:
-        dfs(graph, i, visited)
-        cnt += 1
- 
-print(cnt)
+    if not visited[i]:
+        dfs(i)
+        connected_component += 1
 
+print(connected_component)
