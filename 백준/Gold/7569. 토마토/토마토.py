d2f6f95b2fbc6f57e1bf1c9a6beac9f34b7fd56e
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2025-07-05 22:43:59 +0900

    [Gold V] Title: 토마토, Time: 1812 ms, Memory: 58524 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -3,7 +3,7 @@ from collections import deque
 m, n, h = map(int, input().split())
 
 boxes = []
-visited = [[[-1] * m for _ in range(n)] for _ in range(h)]
+count = [[[-1] * m for _ in range(n)] for _ in range(h)]
 for _ in range(h):
     box = []
     for _ in range(n):
@@ -18,7 +18,7 @@ def bfs():
             for k, item in enumerate(row):
                 if item == 1:
                     queue.append((i, j, k))
-                    visited[i][j][k] = 0
+                    count[i][j][k] = 0
 
     maximum = 0
     while queue:
@@ -27,11 +27,10 @@ def bfs():
         for dx, dy, dz in [(0, 0, 1), (0, 0, -1), (0, 1, 0), (0, -1, 0), (1, 0, 0), (-1, 0, 0)]:
             nx, ny, nz = x + dx, y + dy, z + dz
 
-            if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m and visited[nx][ny][nz] == -1 and boxes[nx][ny][nz] == 0:
+            if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m and boxes[nx][ny][nz] == 0:
                 queue.append((nx, ny, nz))
                 boxes[nx][ny][nz] = -1
-                visited[nx][ny][nz] = visited[x][y][z] + 1
-                maximum = max(maximum, visited[nx][ny][nz])
+                count[nx][ny][nz] = maximum = count[x][y][z] + 1
 
     for i, box in enumerate(boxes):
         for j, row in enumerate(box):
