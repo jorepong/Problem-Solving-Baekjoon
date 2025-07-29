b92093fa4d85170ee1afafabad80fdf13876a8af
Cho Hyunbin <79201348+jorepong@users.noreply.github.com>
2025-07-29 20:29:52 +0900

    [Gold V] Title: 용액, Time: 144 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -9,13 +9,15 @@ end = len(nums) - 1
 ans = [nums[start], nums[end]]
 closest_num = ans[0] + ans[1]
 
-while start + 1 != end:
-    if abs(nums[start + 1] + nums[end]) < abs(nums[start] + nums[end - 1]):
-        start += 1
-    elif abs(nums[start + 1] + nums[end]) >= abs(nums[start] + nums[end - 1]):
+while start < end:
+    if nums[start] + nums[end] > 0:
         end -= 1
+    elif nums[start] + nums[end] < 0:
+        start += 1
+    else:
+        break
 
-    if abs(nums[start] + nums[end]) < abs(closest_num):
+    if start < end and abs(nums[start] + nums[end]) < abs(closest_num):
         closest_num = nums[start] + nums[end]
         ans = [nums[start], nums[end]]
 
